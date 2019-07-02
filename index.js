0d59a4ed6dd202a86409a5f63c42b1a4769f9770
Alejandro Howez <aech-12@hotmail.com>
2019-07-02 15:14:31 -0300

    Fully working backend with v0.2

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+let notes = [
+  { content: 'first', id: 1, important: false },
+  { content: 'second', id: 2, important: true },
+  { content: 'third', id: 3, important: true }
+];
+
+app.get('/', (req, res) => {
+  res.json('hi');
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+app.post('/api/notes', (req, res) => {
+  if (!req.body) {
+    res.status(400).json({ error: 'content missing' });
+  }
+  const { content, important, id } = req.body;
+  const note = {
+    content,
+    id,
+    important: important || false,
+    date: new Date()
+  };
+  notes = [...notes, note];
+  res.json(notes);
+});
+
+app.get('/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find(n => n.id === id);
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+app.put('/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const newNotes = notes.map(n =>
+    n.id !== id ? n : { ...n, important: !n.important }
+  );
+  notes = newNotes;
+  res.json(notes);
+});
+app.delete('/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const newNotes = notes.filter(n => n.id !== id);
+  notes = newNotes;
+  res.status(204).end();
+  s;
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Listening to ${PORT}`);
+});
