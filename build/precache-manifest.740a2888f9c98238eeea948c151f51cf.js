0f1aba370bfd022d8b646792723347526efe11d0
Alejandro Howez <aech-12@hotmail.com>
2019-07-04 10:49:32 -0300

    added static files

---
+++ build/precache-manifest.740a2888f9c98238eeea948c151f51cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5844197c5485c887c16141581cbe79a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a54c256891348d6e4974",
+    "url": "/static/css/main.9e72e3ba.chunk.css"
+  },
+  {
+    "revision": "3041e60f807c19388132",
+    "url": "/static/js/2.9410414e.chunk.js"
+  },
+  {
+    "revision": "a54c256891348d6e4974",
+    "url": "/static/js/main.95637999.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
+app.use(express.static('build'));
 app.use(bodyParser.json());
 app.use(cors());
 
