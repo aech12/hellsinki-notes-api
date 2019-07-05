b566e5b0120782e3fcb402161e7cc224b4f841ce
Alejandro Howez <aech-12@hotmail.com>
2019-07-05 15:34:06 -0300

    0.4 integrating mongodb, get and post notes work

--- README.md
+++ README.md
@@ -3,6 +3,5 @@ Helsinki Fullstack Notes List - Backend
 Working on:
 
 To work on:
-0.2
-
 0.3
+POST /notes -> Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const DB_USER = process.env.DB_USER,
+  DB_PASS = process.env.DB_PASS,
+  DB = 'helsinki-app';
+mongoose
+  .connect(
+    `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0-mxckr.mongodb.net/${DB}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('DB connected'))
+  .catch(e => console.error(console, 'connection error: ', e));
+const db = mongoose.connection;
+
+const notesSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+  date: Date
+});
+const Note = mongoose.model('Note', notesSchema);
+
+// const firstNote = new Note({
+//   content: 'doing it again',
+//   date: new Date(),
+//   important: true
+// });
+// firstNote.save().then(r=> function(err, firstNote) {
+//   if (err) return console.error(err);
+//   mongoose.connection.close();
+// });
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -17,32 +48,49 @@ app.get('/', (req, res) => {
   res.json('hi');
 });
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes);
+app.get('/api/notes', async (req, res) => {
+  try {
+    const notes = await Note.find({}).map(note => {
+      return note;
+    });
+    mongoose.connection.close();
+    res.json(notes);
+  } catch (e) {
+    res.status(404).json({ e: 'Could not find notes: ', e });
+  }
 });
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', async (req, res) => {
   if (!req.body) {
     res.status(400).json({ error: 'content missing' });
   }
   const { content, important, id } = req.body;
-  const note = {
+  const note = new Note({
     content,
     id,
     important: important || false,
     date: new Date()
-  };
-  notes = [...notes, note];
+  });
+  try {
+    const newnote = await note.save();
+    mongoose.connection.close();
+    res.status(200).json(newnote);
+  } catch (e) {
+    res.status(404).json('error: ', e);
+  }
   res.json(notes);
 });
 
 app.get('/notes/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find(n => n.id === id);
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(404).end();
-  }
+  // const id = Number(req.params.id);
+  const id = req.params.id;
+  Note.findById({ ObjectId: id }).then(r => console.log(r));
+  mongoose.connection.close();
+  // const note = notes.find(n => n.id === id);
+  // if (note) {
+  //   res.json(note);
+  // } else {
+  //   res.status(404).end();
+  // }
 });
 app.put('/notes/:id', (req, res) => {
   const id = Number(req.params.id);
